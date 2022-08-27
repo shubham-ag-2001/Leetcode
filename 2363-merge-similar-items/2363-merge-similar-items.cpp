0fe35c5cfe7ef760bb32e330ca249bc65e4c4fb9
Shubham Agarwal <106271248+shubham-ag-2001@users.noreply.github.com>
2022-08-27 16:00:29 +0530

    Time: 220 ms (5.01%), Space: 16 MB (91.66%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        
+        
+        vector<vector<int>> ret ;
+        
+        
+        // vector <pair<int,int>> ret;
+        pair<int, int> p;
+        
+        // vector<int> a;
+        
+        int l1 = items1.size();
+        int l2 = items2.size();
+        
+        
+        for(int i=0; i<l1; i++){
+            vector<int> a;
+            int b=0;
+            // p.first=items1[i][0];
+            
+            for(int j=0; j<l2; j++){
+                if(items2[j][0]==items1[i][0]){
+                    // p.second= (items1[i][1]+items2[j][1]);
+                    b++;
+                    a.push_back(items1[i][0]);
+                    a.push_back((items1[i][1]+items2[j][1]));
+                }
+            }
+            if(!b){
+                // a.push_back(items1[i][0]);
+                // a.push_back(items1[i][1]);
+                ret.push_back(items1[i]);
+            }
+            else{
+                ret.push_back(a);
+            }
+        }
+        
+        
+        for(int i=0; i<l2; i++){
+            int c=0;
+            for(int j=0; j<l1; j++){  
+                if(items1[j][0]==items2[i][0]){
+                    c++;
+                    break;
+                }
+            }
+            if(!c){
+                    ret.push_back(items2[i]);
+                }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    sort(ret.begin(),ret.end());
+    return ret;
+        
+    }
+};
